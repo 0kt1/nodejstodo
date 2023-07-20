93adf18ddd89a0db3661b4298ce0f50a18d95e9a
kaustubh <kaustubhutitare@gmail.com>
2023-07-20 19:43:31 +0530

    commit4

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-var express = require('express');
 
-var app = express();
-
-app.set('view engine', 'ejs');
-
-app.use('/assets', express.static('./public/assets'));
-
-app.listen(3001)
-console.log('You are listening to port 3001')
-
-app.get('/todo', function(req, res){
-    res.render('todo', {todos: req.params.item})
-})
